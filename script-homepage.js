55615a911a190fbbda3533c59120857514510f04
CassieAtaide <cassi.891@hotmail.com>
2025-05-17 21:28:47 -0400

    Update script-homepage.js

--- script-homepage.js
+++ script-homepage.js
@@ -1,20 +1,29 @@
 function sair() {
-    localStorage.removeItem("username");
-    window.location.href = "login.html";
+  localStorage.removeItem("username");
+  window.location.href = "login.html";
+}
+
+function mostrarAjuda() {
+  document.getElementById("ajuda-modal").style.display = "block";
+}
+
+function fecharAjuda() {
+  document.getElementById("ajuda-modal").style.display = "none";
+}
+
+window.onclick = function(event) {
+  const modal = document.getElementById("ajuda-modal");
+  if (event.target == modal) {
+    modal.style.display = "none";
   }
-  
-  function mostrarAjuda() {
-    document.getElementById("ajuda-modal").style.display = "block";
-  }
-  
-  function fecharAjuda() {
-    document.getElementById("ajuda-modal").style.display = "none";
-  }
-  
-  window.onclick = function(event) {
-    const modal = document.getElementById("ajuda-modal");
-    if (event.target == modal) {
-      modal.style.display = "none";
-    }
-  }
+}
+
+
+
+const nome = localStorage.getItem("username");
+if (nome) {
+  document.querySelector(".logo-nav").textContent = `Bem-vindo(a), ${nome}!`;
+}
+
+
   
