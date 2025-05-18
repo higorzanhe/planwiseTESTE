89a4cce0344bc5650ff2b9bca0892c88e1815012
CassieAtaide <cassi.891@hotmail.com>
2025-05-17 21:08:36 -0400

    Update script-homepage.js

--- script-homepage.js
+++ script-homepage.js
@@ -1,1 +1,10 @@
+  <script>
+  function sair() {
+    localStorage.clear();
+    window.location.href = "index-homepage.html";
+  }
 
+  function mostrarAjuda() {
+    alert("Bem-vindo(a) à página principal!\n\n• Use os cards para navegar entre Gerenciamento, Calendário e Finanças.\n• Clique em 'Sair' para encerrar sua sessão.\n• Mais dúvidas? Consulte nosso FAQ em breve!");
+  }
+</script>
